98a7e322bfb8125739cfc63d1c9caf61d7987398
ShvydkyiOleksandr <97990538+ShvydkyiOleksandr@users.noreply.github.com>
2022-04-30 00:47:16 +0300

    17. Cell GradientView

--- Chat/Controllers/Cell/ActiveChatCell.swift
+++ Chat/Controllers/Cell/ActiveChatCell.swift
@@ -18,7 +18,7 @@ class ActiveChatCell: UICollectionViewCell, SelfConfiguringCell {
     let friendImageView = UIImageView()
     let friendName = UILabel(text: "User name", font: .LaoSangamMN20())
     let lastMessage = UILabel(text: "How are you", font: .LaoSangamMN18())
-    let gradientView = UIView()
+    let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: #colorLiteral(red: 0.8309458494, green: 0.7057176232, blue: 0.9536159635, alpha: 1), endColor: #colorLiteral(red: 0.4784313725, green: 0.6980392157, blue: 0.9215686275, alpha: 1))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
